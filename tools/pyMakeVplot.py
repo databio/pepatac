5e26c4ff7d769a5289c2b0bda2e23808dff35be5
nsheff <nsheff@users.noreply.github.com>
2017-03-01 14:11:07 -0500

    Fix pyMakeVplot.py
    Fixes tabs/spaces indent issues.
    Also correctly handles case that threw an error if
    the region file contained chromosomes not present
    in the bam file.

--- tools/pyMakeVplot.py
+++ tools/pyMakeVplot.py
@@ -49,7 +49,7 @@ def asn_mat(val,mat,s_int,e_int,t,i,weight):
         if len(p1_ints[0]) == 3:
             mat[t][base] += weight
         elif p1_ints[i][int(options.s)-1] == "-":
-            mat[t][len(mat[0])-base-1] += weight	
+            mat[t][len(mat[0])-base-1] += weight        
         else:
             mat[t][base] += weight
     return mat
@@ -67,31 +67,36 @@ def sub_Mat(start):
         s_int=center-int(options.e)
         e_int=center+int(options.e)
         # loop through rds
-        for p2_rds in bamfile.fetch(str(p1_ints[i][0]), max(0,s_int-2000), e_int+2000):
-            #check mapping quality
-            if p2_rds.mapq<30:# or p2_rds.is_proper_pair==False:
-                continue
-            # get read positions
-            if p2_rds.is_reverse:
-		continue
-            else:
-		l_pos = p2_rds.pos+4
-                # calculate center point
-		ilen = abs(p2_rds.tlen)-9
-                #ilen = 1
-		r_pos=l_pos+ilen
-		c_pos=l_pos+ilen/2
-		if ilen%2==1 and options.p=='center':
-			mat=asn_mat(c_pos,mat,s_int,e_int,ilen,i,0.5)
-			mat=asn_mat(c_pos+1,mat,s_int,e_int,ilen,i,0.5)
-		elif ilen%2!=1 and options.p=='center':
-			mat=asn_mat(c_pos,mat,s_int,e_int,ilen,i,1)
-		# save ends or read centers to v-plot
-		elif options.p == 'ends':
-			mat = asn_mat(l_pos,mat,s_int,e_int,ilen,i,1)
-			mat = asn_mat(r_pos,mat,s_int,e_int,ilen,i,1)
-		else:
-			sys.exit('Error, check parameters')
+        try:
+            for p2_rds in bamfile.fetch(str(p1_ints[i][0]), max(0,s_int-2000), e_int+2000):
+                #check mapping quality
+                if p2_rds.mapq<30:# or p2_rds.is_proper_pair==False:
+                    continue
+                # get read positions
+                if p2_rds.is_reverse:
+                    continue
+                else:
+                    l_pos = p2_rds.pos+4
+                    # calculate center point
+                    ilen = abs(p2_rds.tlen)-9
+                    #ilen = 1
+                    r_pos=l_pos+ilen
+                    c_pos=l_pos+ilen/2
+                    if ilen%2==1 and options.p=='center':
+                        mat=asn_mat(c_pos,mat,s_int,e_int,ilen,i,0.5)
+                        mat=asn_mat(c_pos+1,mat,s_int,e_int,ilen,i,0.5)
+                    elif ilen%2!=1 and options.p=='center':
+                        mat=asn_mat(c_pos,mat,s_int,e_int,ilen,i,1)
+                    # save ends or read centers to v-plot
+                    elif options.p == 'ends':
+                        mat = asn_mat(l_pos,mat,s_int,e_int,ilen,i,1)
+                        mat = asn_mat(r_pos,mat,s_int,e_int,ilen,i,1)
+                    else:
+                        sys.exit('Error, check parameters')
+        except ValueError:
+            # This happens if the region file has a chromosome not present in the bam file.
+            pass
+
     return mat
 
 ##### INPUTS AND OUTPUTS #####
