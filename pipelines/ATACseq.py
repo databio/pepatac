2a54f9eab2cc956803f42e70196519157d287850
Vince Reuter <vince.reuter@gmail.com>
2017-10-13 11:39:11 -0400

    update future way to add all pypiper args

--- pipelines/ATACseq.py
+++ pipelines/ATACseq.py
@@ -29,7 +29,7 @@ def parse_arguments():
 	# #######################################################################################
 	parser = ArgumentParser(description='Pipeline')
 	parser = pypiper.add_pypiper_args(parser, all_args = True)
-	#parser = pypiper.add_pypiper_args(parser, groups = ['all'])  # future version
+	#parser = pypiper.add_pypiper_args(parser, all_args=True)  # future version
 
 	# Pipeline-specific arguments
 	parser.add_argument("-gs", "--genome-size", default="hs", type=str,
