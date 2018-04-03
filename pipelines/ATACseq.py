219757ac31393ce3f090f6ce3208d383780c819b
jpsmith5 <jasonsmith@virginia.edu>
2018-04-03 07:06:05 -0400

    Fix title case

--- pipelines/ATACseq.py
+++ pipelines/ATACseq.py
@@ -496,11 +496,11 @@ def main():
 		rr = float(pm.get_stat("Raw_reads"))
 		tr = float(pm.get_stat("Trimmed_reads"))
 		dr = float(ar) - float(pdar)
-		pm.report_result("Duplicate_Reads", dr)
-		pm.report_result("Dedup_Aligned_Reads", pdar)
-		pm.report_result("Dedup_Alignment_Rate",
+		pm.report_result("Duplicate_reads", dr)
+		pm.report_result("Dedup_aligned_reads", pdar)
+		pm.report_result("Dedup_alignment_rate",
 		                 round(float(pdar) * 100 / float(tr), 2))
-		pm.report_result("Dedup_Total_Efficiency",
+		pm.report_result("Dedup_total_efficiency",
 		                 round(float(pdar) * 100 / float(rr), 2))    
    
 	rmdup_bam =  os.path.join(map_genome_folder, args.sample_name + ".pe.q10.sort.rmdup.bam")
