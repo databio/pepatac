805a190150a26f63697282038febdad6d6aafc33
jpsmith5 <jasonsmith@virginia.edu>
2018-06-04 11:48:55 -0400

    Use project_config.yaml file as start point in reference to issue #44

--- tools/ATAC_Looper_Summary_plot.R
+++ tools/ATAC_Looper_Summary_plot.R
@@ -1,6 +1,6 @@
 ###############################################################################
 #5/18/17
-#Last Updated 05/18/2018
+#Last Updated 06/04/2018
 #Original Author: Ryan Corces
 #Updated by: Jason Smith
 #ATAC_Looper_Summary_plot.R
@@ -10,7 +10,7 @@
 #
 #NOTES:
 #usage: Rscript /path/to/Rscript/ATAC_Looper_Summary_plot.R 
-#       /path/to/ATACseq_stats_summary.tsv
+#       /path/to/project_config.yaml
 #       --pre human_alphasat rCRSd human_alu human_rDNA human_repeats
 #
 #requirements: argparser, ggplot2, gplots, grid, reshape2, data.table
@@ -19,7 +19,7 @@
 
 ##### LOAD ARGUMENTPARSER #####
 if(suppressPackageStartupMessages(!require(argparser))) {
-  install.packages("argparser")
+    install.packages("argparser")
 }
 suppressPackageStartupMessages(library(argparser, quietly=TRUE))
 
@@ -27,8 +27,8 @@ suppressPackageStartupMessages(library(argparser, quietly=TRUE))
 p <- arg_parser("Produce ATACseq Summary Plots")
 
 # Add command line arguments
-p <- add_argument(p, "stats", 
-                  help="PEPATAC *_stats_summary.tsv")
+p <- add_argument(p, "config", 
+                  help="PEPATAC project_config.yaml")
 p <- add_argument(p, "--pre", 
                   help="[OPT] Specify names of prealignments used", nargs=Inf)
 
@@ -39,19 +39,19 @@ argv <- parse_args(p)
 
 ##### LOAD DEPENDENCIES #####
 if(suppressPackageStartupMessages(!require(ggplot2))) {
-  install.packages("ggplot2")
+    install.packages("ggplot2")
 }
 if(suppressPackageStartupMessages(!require(gplots))) {
-  install.packages("gplots")
+    install.packages("gplots")
 }
 if(suppressPackageStartupMessages(!require(grid))) {
-  install.packages("grid")
+    install.packages("grid")
 }
 if(suppressPackageStartupMessages(!require(reshape2))) {
-  install.packages("reshape2")
+    install.packages("reshape2")
 }
 if(suppressPackageStartupMessages(!require(data.table))) {
-  install.packages("data.table")
+    install.packages("data.table")
 }
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(gplots))
@@ -68,27 +68,27 @@ TSS_CUTOFF <- 6
 
 # Theme for all plots
 alignTheme<-theme(
-  plot.background = element_blank(),
-  panel.grid.major = element_blank(),
-  panel.grid.minor = element_blank(),
-  panel.border = element_rect(colour = "black", fill=NA, size=0.5),
-  panel.background = element_blank(),
-  axis.line = element_blank(),
-  axis.text.x = element_text(face = "plain", color = "black", 
-                             size = 20, hjust = 0.5),
-  axis.text.y = element_text(face = "plain", color = "black",
-                             size = 20, hjust = 1),
-  axis.title.x = element_text(face = "plain", color = "black", 
-                              size = 22, hjust = 0.5, vjust=0.5),
-  axis.title.y = element_text(face = "plain", color = "black", 
-                              size = 22, hjust = 0.5),
-  plot.title = element_text(face="bold", color = "black", 
-                            size=12, hjust=0.5),
-  axis.ticks = element_line(size = 0.5),
-  axis.ticks.length = unit(2, "mm"),
-  legend.background = element_rect(fill="transparent"),
-  legend.text = element_text(size=16),
-  legend.title = element_blank()
+    plot.background  = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border     = element_rect(colour = "black", fill=NA, size=0.5),
+    panel.background = element_blank(),
+    axis.line    = element_blank(),
+    axis.text.x  = element_text(face = "plain", color = "black", 
+                                size = 20, hjust = 0.5),
+    axis.text.y  = element_text(face = "plain", color = "black",
+                                size = 20, hjust = 1),
+    axis.title.x = element_text(face = "plain", color = "black", 
+                                size = 22, hjust = 0.5, vjust=0.5),
+    axis.title.y = element_text(face = "plain", color = "black", 
+                                size = 22, hjust = 0.5),
+    plot.title   = element_text(face="bold", color = "black", 
+                                size=12, hjust=0.5),
+    axis.ticks   = element_line(size = 0.5),
+    axis.ticks.length = unit(2, "mm"),
+    legend.background = element_rect(fill="transparent"),
+    legend.text  = element_text(size=16),
+    legend.title = element_blank()
 )
 
 ###############################################################################
@@ -97,8 +97,8 @@ alignTheme<-theme(
 
 # Taken from https://stackoverflow.com/questions/6461209/how-to-round-up-to-the-nearest-10-or-100-or-x
 roundUpNice <- function(x, nice=c(1,2,3,4,5,6,7,8,9,10)) {
-  if(length(x) != 1) stop("'x' must be of length 1")
-  10^floor(log10(x)) * nice[[which(x <= 10^floor(log10(x)) * nice)[[1]]]]
+    if(length(x) != 1) stop("'x' must be of length 1")
+    10^floor(log10(x)) * nice[[which(x <= 10^floor(log10(x)) * nice)[[1]]]]
 }
 
 # Taken from https://github.com/baptiste/egg/blob/master/R/set_panel_size.r
@@ -106,45 +106,53 @@ set_panel_size <- function(p=NULL, g=ggplotGrob(p), file=NULL,
                            margin=unit(1, "in"),
                            width=unit(4, "in"), 
                            height=unit(4, "in")){
-  
-  panels <- grep("panel", g$layout$name)
-  panel_index_w <- unique(g$layout$l[panels])
-  panel_index_h <- unique(g$layout$t[panels])
-  nw <- length(panel_index_w)
-  nh <- length(panel_index_h)
-  
-  if(getRversion() < "3.3.0"){
     
-    # the following conversion is necessary
-    # because there is no `[<-`.unit method
-    # so promoting to unit.list allows standard list indexing
-    g$widths  <- grid:::unit.list(g$widths)
-    g$heights <- grid:::unit.list(g$heights)
+    panels <- grep("panel", g$layout$name)
+    panel_index_w <- unique(g$layout$l[panels])
+    panel_index_h <- unique(g$layout$t[panels])
+    nw <- length(panel_index_w)
+    nh <- length(panel_index_h)
     
-    g$widths[panel_index_w]  <- rep(list(width), nw)
-    g$heights[panel_index_h] <- rep(list(height), nh)
+    if(getRversion() < "3.3.0"){
+        
+        # the following conversion is necessary
+        # because there is no `[<-`.unit method
+        # so promoting to unit.list allows standard list indexing
+        g$widths  <- grid:::unit.list(g$widths)
+        g$heights <- grid:::unit.list(g$heights)
+        
+        g$widths[panel_index_w]  <- rep(list(width), nw)
+        g$heights[panel_index_h] <- rep(list(height), nh)
+        
+    } else {
+        
+        g$widths[panel_index_w]  <- rep(width, nw)
+        g$heights[panel_index_h] <- rep(height, nh)
+        
+    }
     
-  } else {
+    if(!is.null(file))
+        ggsave(file, g, limitsize = FALSE,
+               width=convertWidth(sum(g$widths) + margin, 
+                                  unitTo="in", valueOnly=TRUE),
+               height=convertHeight(sum(g$heights) + margin,  
+                                    unitTo="in", valueOnly=TRUE))
     
-    g$widths[panel_index_w]  <- rep(width, nw)
-    g$heights[panel_index_h] <- rep(height, nh)
-    
-  }
-  
-  if(!is.null(file))
-    ggsave(file, g, limitsize = FALSE,
-           width=convertWidth(sum(g$widths) + margin, 
-                              unitTo="in", valueOnly=TRUE),
-           height=convertHeight(sum(g$heights) + margin,  
-                                unitTo="in", valueOnly=TRUE))
-  
-  invisible(g)
+    invisible(g)
 }
 
 ###############################################################################
 
-summaryFile <- argv$stats
+configFile <- argv$config
+# read in project_config.yaml file
+    configYaml <- fread(configFile, header=TRUE, check.names=FALSE,
+                        sep="\t", quote="")
+colnames(configYaml) <- "config"
 
+# expand output directory location to identify the stats_summary.tsv file
+summaryFile <- system(paste("echo ", paste(gsub("output_dir: ", "",
+                      configYaml$config[grep("output_dir", configYaml$config)]),
+                      "/*_stats_summary.tsv", sep=""), sep=""), intern=TRUE)
 setwd(dirname(summaryFile))
 
 # read in stats summary file
@@ -154,28 +162,29 @@ stats <- fread(summaryFile, header=TRUE, check.names=FALSE, sep="\t", quote="")
 if (!is.na(argv$pre)) {
     prealignments <- argv$pre
 } else {
-    prealignments <- gsub("Aligned_reads_", "", grep("Aligned_reads_.*", 
-                          colnames(stats), value=TRUE))
+    prealignments <- gsub("Aligned_reads_", "",
+                          grep("Aligned_reads_.*", colnames(stats),
+                          value=TRUE))
 }
 
 # confirm the provided prealignments exist in stats_summary.tsv
 for (i in 1:length(prealignments)) {
-  # get number of prealignments in stats_summary.tsv file
-  if(length(grep("Aligned_reads_.*", colnames(stats))) != length(prealignments)){
-    cat("\n")
-    errorMessage <- paste("There are additional prealignments present in ", 
-                          summaryFile, ".  ",
-                          "\nConfirm the prealignments you performed.",
-                          sep="", collapse="\n")
-    stop(errorMessage)
-  } else if (!(paste("Aligned_reads", prealignments[i], sep="_") 
-               %in% colnames(stats))) {
-    errorMessage <- paste(prealignments[i],  
-                          " is not present in ", summaryFile, ".  ",
-                          "\nConfirm the prealignments you performed.",
-                          sep="", collapse="\n")
-    stop(errorMessage)
-  }
+    # get number of prealignments in stats_summary.tsv file
+    if(length(grep("Aligned_reads_.*", colnames(stats))) != length(prealignments)){
+        cat("\n")
+        errorMessage <- paste("There are additional prealignments present in ", 
+                              summaryFile, ".  ",
+                              "\nConfirm the prealignments you performed.",
+                              sep="", collapse="\n")
+        stop(errorMessage)
+    } else if (!(paste("Aligned_reads", prealignments[i], sep="_") 
+                 %in% colnames(stats))) {
+        errorMessage <- paste(prealignments[i],  
+                              " is not present in ", summaryFile, ".  ",
+                              "\nConfirm the prealignments you performed.",
+                              sep="", collapse="\n")
+        stop(errorMessage)
+    }
 }
 
 write(paste("\nGenerating plots in pdf format with ", 
@@ -214,8 +223,8 @@ for (i in 1:length(genomeNames)) {
 
 for (i in 1:length(prealignments)) {
     alignRaw[, (prealignments[i]) := 
-             stats[, (paste("Aligned_reads", prealignments[i], sep="_")),
-                   with=FALSE][[1]]]
+                 stats[, (paste("Aligned_reads", prealignments[i], sep="_")),
+                       with=FALSE][[1]]]
 }
 
 setcolorder(alignRaw, c("sample", "Unaligned", paste(prealignments),
@@ -270,7 +279,7 @@ for (i in 1:length(prealignments)) {
 alignPercent <- data.table(sample=stats$sample_name,
                            Unaligned=Unaligned,
                            Duplicates=stats$Alignment_rate - 
-                                      stats$Dedup_alignment_rate)
+                               stats$Dedup_alignment_rate)
 
 # Split percents based on genome name
 genomeNames <- unique(stats$Genome)
@@ -285,9 +294,10 @@ for (i in 1:length(genomeNames)) {
 }
 for (i in 1:length(prealignments)) {
     alignPercent[, 
-        (prealignments[i]) := 
-        stats[, (paste("Alignment_rate", prealignments[i], sep="_")), 
-              with=FALSE][[1]]]
+                 (prealignments[i]) := 
+                     stats[, (paste("Alignment_rate", prealignments[i],
+                                    sep="_")), 
+                           with=FALSE][[1]]]
 }
 
 setcolorder(alignPercent, c("sample", "Unaligned", paste(prealignments),
@@ -377,31 +387,31 @@ set_panel_size(
 
 ##### LIBRARY SIZE PLOT #####
 if (any(!is.na(stats$Picard_est_lib_size))) {
-  PicardLibSize <- cbind.data.frame(
-                       sample=stats$sample_name, 
-                       LibSize=(as.numeric(stats$Picard_est_lib_size)/1000000))
-  maxSize       <- max(PicardLibSize$LibSize)
-  upperLimit    <- roundUpNice(maxSize)
-  chartHeight   <- (length(unique(PicardLibSize$sample))) * 0.75
-  
-  PicardLibSize$sample <- factor(PicardLibSize$sample, 
-                                 levels = PicardLibSize$sample)
-  
-  LibSizePlot <- ggplot(PicardLibSize, 
-                        aes(x = sample, y = as.numeric(LibSize))) +
-    geom_col(colour="black", size = 0.25, width=0.8, 
-             fill = "royalblue1") + 
-    labs(x = "", y = "Picard Library Size (M)") +
-    scale_x_discrete(limits = rev(levels(PicardLibSize$sample))) +
-    scale_y_continuous(limits = c(0,upperLimit), expand=c(0,0)) +
-    coord_flip() + 
-    alignTheme
-  
-  set_panel_size(LibSizePlot, 
-                 file=gsub(pattern=".tsv", replacement=".LibSize.pdf", 
-                           x=summaryFile), 
-                 width=unit(8,"inches"), 
-                 height=unit(chartHeight,"inches"))
+    PicardLibSize <- cbind.data.frame(
+        sample=stats$sample_name, 
+        LibSize=(as.numeric(stats$Picard_est_lib_size)/1000000))
+    maxSize       <- max(PicardLibSize$LibSize)
+    upperLimit    <- roundUpNice(maxSize)
+    chartHeight   <- (length(unique(PicardLibSize$sample))) * 0.75
+    
+    PicardLibSize$sample <- factor(PicardLibSize$sample, 
+                                   levels = PicardLibSize$sample)
+    
+    LibSizePlot <- ggplot(PicardLibSize, 
+                          aes(x = sample, y = as.numeric(LibSize))) +
+        geom_col(colour="black", size = 0.25, width=0.8, 
+                 fill = "royalblue1") + 
+        labs(x = "", y = "Picard Library Size (M)") +
+        scale_x_discrete(limits = rev(levels(PicardLibSize$sample))) +
+        scale_y_continuous(limits = c(0,upperLimit), expand=c(0,0)) +
+        coord_flip() + 
+        alignTheme
+    
+    set_panel_size(LibSizePlot, 
+                   file=gsub(pattern=".tsv", replacement=".LibSize.pdf", 
+                             x=summaryFile), 
+                   width=unit(8,"inches"), 
+                   height=unit(chartHeight,"inches"))
 } else {
     quit()
 }
