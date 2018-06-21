4ed755240085ff8da3433e928615c9a63db4f5ad
jpsmith5 <jasonsmith@virginia.edu>
2018-06-21 08:27:14 -0400

    Drop paired-end required true commenting

--- pipelines/ATACseq.py
+++ pipelines/ATACseq.py
@@ -299,8 +299,6 @@ def main():
     args = parse_arguments()
 
     args.paired_end = args.single_or_paired == "paired"
-    # TODO: for now, paired end sequencing input is required.
-    # args.paired_end = True
 
     # Initialize, creating global PipelineManager and NGSTk instance for
     # access in ancillary functions outside of main().
