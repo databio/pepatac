3ca1738c77ec633fbf10932f87aa30d201630fea
nsheff <nsheff@users.noreply.github.com>
2019-04-17 09:39:06 -0400

    Update biocproject function

--- BiocProject/readPepatacPeakBeds.R
+++ BiocProject/readPepatacPeakBeds.R
@@ -3,9 +3,9 @@ readPepatacPeakBeds = function(project) {
   DEFAULT_GRANGES_COLS = c('chr', 'start', 'end')
   # inferring the suffix, which is "peak_calling_" + genome_assembly,
   # see: pepatac.py
-  if (length(unique(samples(p)$genome)) != 1)
+  if (length(unique(samples(project)$genome)) != 1)
     stop(paste0("Need one genome assembly, got  ",
-                length(unique(samples(p)$genome)),
+                length(unique(samples(project)$genome)),
                 ".\nCouldn't infer the path to the files."))
   genome_assembly = unique(samples(project)$genome)
   suffix = paste0("peak_calling_", genome_assembly)
@@ -18,25 +18,20 @@ readPepatacPeakBeds = function(project) {
   # get sample names from PEP
   samples_names = samples(project)$sample_name
   # read the data for each sample
-  result = lapply(samples_names, function(sample) {
+  result = lapply(samples_names, function(sample_name) {
     # use the provided arguments to construct the path
-    dir = file.path(outputDir, prefix, sample, suffix)
+    dir = file.path(prefix, sample_name, suffix)
     # find BED files in the path
-    bedFiles = list.files(path=dir, pattern="*.bed")
+    bedFiles = list.files(path=dir, pattern="*_peaks.bed")
     # get absolute paths to the BED files
     bedFilesAbs = file.path(dir,bedFiles)
     gr = list()
-    # for eache BED file for each sample
-    message("reading ",length(bedFiles)," files for sample: ", sample)
+    # for eache BED file for each sample_name
+    message("reading ",length(bedFiles)," files for sample: ", sample_name)
     for (i in seq_along(bedFilesAbs)) {
       # read BED file
       df = read.table(bedFilesAbs[i])
-      # since the number of columns varies, name the first 3 as default and 
-      # the rest metadataX
-      colNames = append(
-        DEFAULT_GRANGES_COLS,
-        paste0("metadata", seq(1,NCOL(df)-length(DEFAULT_GRANGES_COLS))))
-      colnames(df) = colNames
+      colnames(df)[1:3] = DEFAULT_GRANGES_COLS
       # convert the data.frame to GenomicRanges::GRanges object
       gr[[i]] = GenomicRanges::GRanges(df)
     }
