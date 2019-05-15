d99ed6b03b2ddc36a0f85af4f31711a644e078f5
jpsmith5 <jasonsmith@virginia.edu>
2019-05-15 11:24:59 -0400

    add new line to message

--- tools/filter_paired_fq.pl
+++ tools/filter_paired_fq.pl
@@ -23,18 +23,25 @@ $file_fq2_filtered = shift;
 open(my $fh_filter, "<", $file_filter);
 # We can't read from compressed input because it messes with the way the files
 # are read in, but this is how you would do it.
-# open(my $fh_fq1, "gunzip -c $file_fq1 |");
-# open(my $fh_fq2, "gunzip -c $file_fq2 |");
+# if ($file_fq1 =~ /\.gz$/i) {
+# 	open(my $fh_fq1, "gunzip -c $file_fq1 |");
+# } else {
+# 	open(my $fh_fq1, "<", $file_fq1);
+# }
+# if ($file_fq2 =~ /\.gz$/i) {
+# 	open(my $fh_fq2, "gunzip -c $file_fq2 |");
+# } else {
+# 	open(my $fh_fq2, "<", $file_fq2);
+# }
 open(my $fh_fq1, "<", $file_fq1);
 open(my $fh_fq2, "<", $file_fq2);
 
-
 # write output files here
 if ($file_fq1_filtered =~ /\.gz$/i) {
-	print STDERR "gzipping output";
+	print STDERR "gzipping output\n";
 	open(FH_FQ1_FILT, "| /bin/gzip -c > $file_fq1_filtered");
 } else {
-	print STDERR "not gzipping output";
+	print STDERR "not gzipping output\n";
 	open(FH_FQ1_FILT, ">", $file_fq1_filtered);
 }
 
