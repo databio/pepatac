0005e95960479c3eea36e52f1b1802ac4e48ef99
jpsmith5 <jasonsmith@virginia.edu>
2020-03-03 13:35:56 -0500

    print plot objects

--- tools/PEPATAC.R
+++ tools/PEPATAC.R
@@ -3,7 +3,7 @@
 # PEPATAC R parser
 
 ###############################################################################
-version <- 0.4
+version <- 0.5
 ##### Load dependencies #####
 
 required_libraries <- c("PEPATACr")
@@ -55,11 +55,13 @@ if (is.na(subcmd) || grepl("/R", subcmd)) {
 } else if (!is.na(subcmd) && tolower(subcmd) == "frif") {
     usage <- paste0(
         "\n",
-        "Usage:   PEPATAC.R [command] {args}\n",
+        "Usage:   PEPPRO.R [command] {args}\n",
         "Version: ", version, "\n\n",
         "Command: frif \t plot fraction of reads in features\n\n",
-        " -n, --sample_name\t   Sample name.\n",
-        " -r, --reads\t\t   Number of mapped reads.\n",
+        " -s, --sample_name\t   Sample name.\n",
+        " -n, --num_reads\t\t   Number of mapped reads.\n",
+        " -z, --size\t\t   Size of genome (bp).\n",
+        " -y, --type\t Choose plot type: cFRiF, FRiF, or Both.\n",
         " -o, --output_name\t   Output file name.\n",
         " -b, --bed\t\t   Coverage file(s).\n"
     )
@@ -75,21 +77,58 @@ if (is.na(subcmd) || grepl("/R", subcmd)) {
         message(usage)
         quit()
     } else {
-        sample_name <- opt_get(name = c("sample_name", "n"), required=TRUE,
+        sample_name <- opt_get(name = c("sample_name", "s"), required=TRUE,
                                description="Sample name.")
-        reads       <- opt_get(name = c("reads", "r"), required=TRUE,
-                               description="Number of mapped reads.")
+        num_reads   <- opt_get(name = c("num_reads", "n"), required=TRUE,
+                               description="Number of mapped reads (or bases).")
+        genome_size <- opt_get(name = c("size", "z"), required=TRUE,
+                               description="Size of genome (bp).")
+        type        <- opt_get(name = c("type", "y"), required=FALSE, default="cfrif",
+                               description="Choose plot type: cFRiF, FRiF, or Both (Default = cfrif).")
+        reads       <- opt_get(name = c("reads", "r"), required=FALSE, default=FALSE,
+                               description="Calculate using reads (TRUE) or bases (FALSE) (Default = FALSE).")
         output_name <- opt_get(name = c("output_name", "o"), required=TRUE,
                                description="Output file name.")
         numArgs     <- length(opt_get_args())
+        argGap      <- ifelse(reads, 13, 12)
         bed         <- opt_get(name = c("bed", "b"), required=TRUE,
-                               n=(numArgs - 8),
+                               n=(numArgs - argGap),
                                description="Coverage file(s).")
 
-        plotFRiF(sample_name = sample_name,
-                 num_reads = reads,
-                 output_name = output_name,
-                 BED_file = bed)
+        p <- plotFRiF(sample_name = sample_name,
+                      num_reads = as.numeric(num_reads),
+                      genome_size = as.numeric(genome_size),
+                      type = tolower(type),
+                      reads = reads,
+                      output_name = output_name,
+                      bedFile = bed)
+
+        if (tolower(type) == "both") {
+            pdf(file = paste0(tools::file_path_sans_ext(output_name), ".pdf"),
+                height = 5.45, width = 8.39, useDingbats=F)
+            suppressWarnings(print(p))
+            invisible(dev.off())
+            png(filename = paste0(tools::file_path_sans_ext(output_name), ".png"),
+                height = 550, width=850)
+            suppressWarnings(print(p))
+            invisible(dev.off())
+        } else {
+            pdf(file = paste0(tools::file_path_sans_ext(output_name), ".pdf"),
+                height = 4, width = 4, useDingbats=F)
+            suppressWarnings(print(p))
+            invisible(dev.off())
+            png(filename = paste0(tools::file_path_sans_ext(output_name), ".png"),
+                height = 275, width=275)
+            suppressWarnings(print(p))
+            invisible(dev.off())
+        }
+        
+
+        if (exists("p")) {
+            write(paste0("Cumulative ", type, " plot completed!\n"), stdout())
+        } else {
+            write(paste0("Unable to produce ", type, " plot!\n"), stdout())
+        }
     }
 } else if (!is.na(subcmd) && tolower(subcmd) == "tss") {
     usage <- paste0(
@@ -130,10 +169,28 @@ if (is.na(subcmd) || grepl("/R", subcmd)) {
             }
         }
 
-        TSSfile    <- opt_get(name = c("input", "i"), required=TRUE, n=inArgs,
-                              description="TSS enrichment file.")
+        TSSfile     <- opt_get(name = c("input", "i"), required=TRUE, n=inArgs,
+                               description="TSS enrichment file.")
 
-        plotTSS(TSS_file = TSSfile)
+        p           <- plotTSS(TSSfile = TSSfile)
+
+        sample_name <- sampleName(TSSfile[1])
+
+        png(filename = paste0(sample_name, "_TSS_enrichment.png"),
+            width = 275, height = 275)
+        suppressWarnings(print(p))
+        invisible(dev.off())
+
+        pdf(file = paste0(sample_name, "_TSS_enrichment.pdf"),
+            width = 4, height = 4, useDingbats=F)
+        suppressWarnings(print(p))
+        invisible(dev.off())
+
+        if (exists("p")) {
+            write("TSS enrichment plot completed!\n", stdout())
+        } else {
+            write("Unable to produce TSS enrichment plot!\n", stdout())
+        }
     }
 } else if (!is.na(subcmd) && tolower(subcmd) == "frag") {
     usage <- paste0(
@@ -162,22 +219,41 @@ if (is.na(subcmd) || grepl("/R", subcmd)) {
                                description="Column of fragment lengths.")
         fragL_count <- opt_get(name = c("frag_count", "c"), required=TRUE,
                                description="Counts of each fragment length.")
-        fragL_dis1  <- opt_get(name = c("pdf", "p"), required=TRUE,
+        fragL_name  <- opt_get(name = c("pdf", "p"), required=TRUE,
                                description="PDF output file name.")
-        fragL_dis2  <- opt_get(name = c("text", "t"), required=TRUE,
+        fragL_txt   <- opt_get(name = c("text", "t"), required=TRUE,
                                description="Fragment length distribution stats file.")
 
-        plotFLD(fragL = fragL,
-                fragL_count = fragL_count,
-                fragL_dis1 = fragL_dis1,
-                fragL_dis2 = fragL_dis2)
+        p <- plotFLD(fragL = fragL,
+                     fragL_count = fragL_count,
+                     fragL_txt = fragL_txt,
+                     max_fragment = 600)
+
+        # Save plot to pdf file
+        pdf(file=fragL_name, width = 4, height = 4, useDingbats=F)
+        suppressWarnings(print(p))
+        invisible(dev.off())
+
+        # Save plot to png file
+        outfile_png <- gsub('pdf', 'png', fragL_name)
+        png(filename=outfile_png, width = 275, height = 275)
+        suppressWarnings(print(p))
+        invisible(dev.off())
+
+        if (exists("p")) {
+            write("Fragment distribution plot completed!\n", stdout())
+        } else {
+            write("Unable to produce fragment distribution plot!\n", stdout())
+        }
     }
 } else if (!is.na(subcmd) && tolower(subcmd) == "anno") {
+    # TODO: split into multiple functions
     usage <- paste0(
         "\n",
         "Usage:   PEPATAC.R [command] {args}\n",
         "Version: ", version, "\n\n",
         "Command: anno \t plot peak and read annotation distributions\n\n",
+        " -p, --plot\t Choose plot: Chromosome, TSS, or Genomic distributions",
         " -i, --input\t Input file to be annotated.\n",
         " -t, --type\t Input type: np (narrowPeak) or bed.\n",
         " -f, --feat\t BED6 file containing genome annotations.\n",
@@ -196,6 +272,9 @@ if (is.na(subcmd) || grepl("/R", subcmd)) {
         message(usage)
         quit()
     } else {
+        plot   <- opt_get(name = c("plot", "p"), required=FALSE,
+                          default="chromosome",
+                          description="Choose plot: Chromosome, TSS, or Genomic distributions (Default = Chromosome).")
         input  <- opt_get(name = c("input", "i"), required=TRUE,
                           description="Input file to be annotated.")
         type   <- opt_get(name = c("type", "t"), required=FALSE,
@@ -206,14 +285,32 @@ if (is.na(subcmd) || grepl("/R", subcmd)) {
         genome <- opt_get(name = c("genome", "g"), required=FALSE,
                           default="hg38",
                           description="Genome name (e.g. hg38).")
-        output <- opt_get(name = c("output", "o"), required=FALSE, default=".",
-                          description="Parent output directory.")
+        output_name <- opt_get(name = c("output", "o"), required=TRUE,
+                               description="Output file name.")
 
-        plotAnno(input = input,
-                 type = type,
-                 feat = feat,
-                 genome = genome,
-                 output = output)
+        p <- plotAnno(plot = plot,
+                      input = input,
+                      type = type,
+                      feat = feat,
+                      genome = genome,
+                      output = output_name)
+
+        pdf(file = paste0(tools::file_path_sans_ext(output_name), ".pdf"),
+            width = 7, height = 7, useDingbats=F)
+        suppressMessages(suppressWarnings(print(p)))
+        invisible(dev.off())
+             
+        # Save plot to png file
+        png(filename = paste0(tools::file_path_sans_ext(output_name), ".png"),
+            width = 480, height = 480)
+        suppressMessages(suppressWarnings(print(p)))
+        invisible(dev.off())
+
+        if (exists("p")) {
+            write(paste0(plot, " distribution plot completed!\n"), stdout())
+        } else {
+            write(paste0("Unable to produce ", plot, " distribution plot!\n"), stdout())
+        }
     }
 } else if (!is.na(subcmd) && tolower(subcmd) == "bigbed") {
     usage <- paste0(
