ee3a7d8f4b9844d47d95eea3639955c4a899fbba
jpsmith5 <jasonsmith@virginia.edu>
2020-11-06 14:29:28 -0500

    clean up debug notes

--- BiocProject/runCOCOA.R
+++ BiocProject/runCOCOA.R
@@ -55,45 +55,15 @@ runCOCOA = function(project, results_subdir, summary_dir,
     loadings         <- pca$rotation
     scores           <- pca$x
     rownames(scores) <- rownames(mat)
-    
-    #message("Produce signal coordinates (peaks)")
-    # Generate signal coordinates
-    # coords <- data.frame(coordinates=rownames(m))
-    # message("coords")
-    # print(coords[1:4,1])
-    # message("rownames(m)")
-    # print(head(rownames(m),4))
-    # message("remove matching rows")
+
     delIdx <- c(grep("random", rownames(m)),
                 grep("GL", rownames(m)),
                 grep("chrUn", rownames(m)))
-    #print(head(delIdx))
-    # coords2 <- coords[!delIdx,]
-    # message("cleaned up coords")
-    # #print(coords2[1:4,1])
-    # message("split column")
-    # eval(setDT(coords)[,
-        # c("chr", "start", "end") := tstrsplit(coords$coordinates, "_",
-        # type.convert = TRUE, fixed = TRUE)], envir=globalenv())
-    #peaks  <- coords[,-1]
     coord_list <- data.table::tstrsplit(rownames(m)[-delIdx], "_",
                                         type.convert = TRUE, fixed = TRUE)
-    
-    #print(data.table::tstrsplit(coords$coordinates, "_", type.convert = TRUE, fixed = TRUE))
-    #message("check class")
-    #message(class(coord_list))
-    #message(length(coord_list))
-    #message("build peaks data.table")
     peaks <- data.table::data.table(chr=coord_list[[1]],
                                     start=coord_list[[2]],
                                     end=coord_list[[3]])
-    #message("peak example:")
-    #print(peaks[1:3,])
-    #peaks  <- eval(separate(coords, col="coordinates", sep="_", into=c("chr", "start", "end")), envir=globalenv())
-    #message("set column type")
-    #peaks$start <- as.numeric(peaks$start)
-    #peaks$end   <- as.numeric(peaks$end)
-
     # Load region database    
     regionSetDB <- suppressWarnings(loadRegionDB(lolaPath))
     # Load the ENCODE transcription factor binding sites
@@ -113,13 +83,13 @@ runCOCOA = function(project, results_subdir, summary_dir,
     correctSampleOrder <- 1:nrow(mat)
     message("run COCOA")
     tfRSS <- local(COCOA::runCOCOA(genomicSignal=loadings,
-                             signalCoord=peaks,
-                             GRList=TFGR,
-                             signalCol=PCsToAnnotate,
-                             sampleOrder=correctSampleOrder,
-                             targetVar=scores,
-                             scoringMetric="proportionWeightedMean",
-                             absVal=TRUE))
+                                   signalCoord=peaks,
+                                   GRList=TFGR,
+                                   signalCol=PCsToAnnotate,
+                                   sampleOrder=correctSampleOrder,
+                                   targetVar=scores,
+                                   scoringMetric="proportionWeightedMean",
+                                   absVal=TRUE))
     tfRSS$regionSetName        <- regionSetName
     tfRSS$regionSetDescription <- regionSetDescription
     return(tfRSS)    
