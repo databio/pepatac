a39caac27423852b197bfa5c37bf649f2f7203d2
jpsmith5 <jasonsmith@virginia.edu>
2021-05-17 13:45:34 -0400

    update version

--- pipelines/pepatac.py
+++ pipelines/pepatac.py
@@ -5,7 +5,7 @@ PEPATAC - ATACseq pipeline
 
 __author__ = ["Jin Xu", "Nathan Sheffield", "Jason Smith"]
 __email__ = "jasonsmith@virginia.edu"
-__version__ = "0.9.15"
+__version__ = "0.9.16"
 
 
 from argparse import ArgumentParser
