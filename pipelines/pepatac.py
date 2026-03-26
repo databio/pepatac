c1cd59a4c541415519812a9fe5618e38a0c0469a
nsheff <nsheff@users.noreply.github.com>
2026-03-25 22:46:12 -0400

    version bump to 0.13.1

--- pipelines/pepatac.py
+++ pipelines/pepatac.py
@@ -6,7 +6,7 @@ PEPATAC - ATACseq pipeline
 __author__ = ["Jin Xu", "Nathan Sheffield", "Jason Smith"]
 __email__ = "jasonsmith@virginia.edu"
 
-__version__ = "0.13.0"
+__version__ = "0.13.1"
 
 
 from argparse import ArgumentParser
